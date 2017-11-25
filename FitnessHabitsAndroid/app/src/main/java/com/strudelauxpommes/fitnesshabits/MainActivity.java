6344695be022f5ebad8c8fba4a6e15489185622e
Samuel Chamberland <sam.chamberland@me.com>
2017-11-25 11:31:53 -0500

    Add listener and instantiate Intent

--- FitnessHabitsAndroid/app/src/main/java/com/strudelauxpommes/fitnesshabits/MainActivity.java
+++ FitnessHabitsAndroid/app/src/main/java/com/strudelauxpommes/fitnesshabits/MainActivity.java
@@ -1,7 +1,12 @@
 package com.strudelauxpommes.fitnesshabits;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.strudelauxpommes.fitnesshabits.sleep.SleepFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button sommeilButton = findViewById(R.id.goToSommeilView);
+
+        sommeilButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SleepFragment fragment = new SleepFragment();
+                android.support.v4.app.FragmentManager fragmentManager = getSupportFragmentManager();
+                fragmentManager.beginTransaction().replace(R.id.frameLayout, fragment).commit();
+
+
+                startActivity(new Intent(MainActivity.this, SleepFragment.class));
+            }
+        });
     }
 }
