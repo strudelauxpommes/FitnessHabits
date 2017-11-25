c9a5ab42980d5edd8d3ac31bb83c5e218122a27b
Samuel Chamberland <sam.chamberland@me.com>
2017-11-25 11:40:15 -0500

    Fix typo

--- FitnessHabitsAndroid/app/src/main/java/com/strudelauxpommes/fitnesshabits/MainActivity.java
+++ FitnessHabitsAndroid/app/src/main/java/com/strudelauxpommes/fitnesshabits/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button sommeilButton = findViewById(R.id.goToSommeilView);
+        Button sommeilButton = findViewById(R.id.SleepFragment);
 
         sommeilButton.setOnClickListener(new View.OnClickListener() {
             @Override
