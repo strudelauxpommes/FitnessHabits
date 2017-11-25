84a507c31dabeaf8f496ef0dc81c6838adb5505b
the-prism <t.castonguay.gagnon@gmail.com>
2017-11-25 10:03:01 -0500

    database init

--- FitnessHabitsAndroid/app/src/main/java/com/strudelauxpommes/fitnesshabits/MainActivity.java
+++ FitnessHabitsAndroid/app/src/main/java/com/strudelauxpommes/fitnesshabits/MainActivity.java
@@ -1,13 +1,31 @@
 package com.strudelauxpommes.fitnesshabits;
 
+import android.arch.persistence.room.Room;
+import android.content.Context;
+import android.support.annotation.MainThread;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.strudelauxpommes.fitnesshabits.data.AppDatabase;
+
 public class MainActivity extends AppCompatActivity {
+
+    public static MainActivity application;
+    private AppDatabase database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @MainThread
+    public AppDatabase getDatabase() {
+        if (database==null){
+            database = Room.databaseBuilder(this, AppDatabase.class,"FitnessHabits-database").fallbackToDestructiveMigration().build(); //TODO: remove fallback destroy
+        }
+        return database;
+    }
+
+    //TODO, repository access HERE
 }
