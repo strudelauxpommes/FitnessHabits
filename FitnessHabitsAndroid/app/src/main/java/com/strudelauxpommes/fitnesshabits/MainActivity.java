b81b7a48aed919299b7f19f17c5e8e526f81c2fe
Samuel Chamberland <sam.chamberland@me.com>
2017-11-25 11:40:15 -0500

    Fix typo

--- FitnessHabitsAndroid/app/src/main/java/com/strudelauxpommes/fitnesshabits/MainActivity.java
+++ FitnessHabitsAndroid/app/src/main/java/com/strudelauxpommes/fitnesshabits/MainActivity.java
@@ -1,13 +1,9 @@
 package com.strudelauxpommes.fitnesshabits;
 
-import android.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
