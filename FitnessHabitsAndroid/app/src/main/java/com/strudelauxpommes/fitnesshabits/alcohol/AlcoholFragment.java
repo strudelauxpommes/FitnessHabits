e7ecbcd735cc7e68aef3337d9723f45fb105d759
Vincent <vi.lirette@gmail.com>
2017-11-25 12:47:29 -0500

    WIP

--- FitnessHabitsAndroid/app/src/main/java/com/strudelauxpommes/fitnesshabits/alcohol/AlcoholFragment.java
+++ FitnessHabitsAndroid/app/src/main/java/com/strudelauxpommes/fitnesshabits/alcohol/AlcoholFragment.java
@@ -3,6 +3,8 @@ package com.strudelauxpommes.fitnesshabits.alcohol;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,8 +15,9 @@ import com.strudelauxpommes.fitnesshabits.R;
  * A simple {@link Fragment} subclass.
  */
 public class AlcoholFragment extends Fragment {
-
-
+    private RecyclerView alcoholRecycler;
+    private RecyclerView.LayoutManager layoutManager;
+    private RecyclerView.Adapter adapter;
     public AlcoholFragment() {
         // Required empty public constructor
     }
@@ -24,7 +27,17 @@ public class AlcoholFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_alcohol, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_alcohol, container, false);
+
+        alcoholRecycler = (RecyclerView)  rootView.findViewById(R.id.alcohol_recycler);
+
+        layoutManager = new LinearLayoutManager(getActivity());
+        alcoholRecycler.setLayoutManager(layoutManager);
+
+        adapter = new AlcoholSummaryAdapter();
+        alcoholRecycler.setAdapter(adapter);
+
+        return rootView;
     }
 
 }
