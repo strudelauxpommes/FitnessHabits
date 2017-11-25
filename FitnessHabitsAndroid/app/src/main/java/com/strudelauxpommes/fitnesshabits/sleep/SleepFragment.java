3c69abb5c9917a3b23b87b78d385a6dd45a4948b
tirvaxx <novis.mathieu@courrier.uqam.ca>
2017-11-25 10:25:39 -0500

    sleep details

--- FitnessHabitsAndroid/app/src/main/java/com/strudelauxpommes/fitnesshabits/sleep/SleepFragment.java
+++ FitnessHabitsAndroid/app/src/main/java/com/strudelauxpommes/fitnesshabits/sleep/SleepFragment.java
@@ -8,11 +8,21 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.strudelauxpommes.fitnesshabits.R;
+import android.widget.TimePicker;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class SleepFragment extends Fragment {
+
 
 
     public SleepFragment() {
@@ -23,8 +33,10 @@ public class SleepFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_sleep, container, false);
     }
+
 
 }
