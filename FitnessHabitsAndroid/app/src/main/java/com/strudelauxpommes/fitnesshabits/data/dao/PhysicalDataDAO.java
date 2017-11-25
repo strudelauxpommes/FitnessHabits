4edff54b1b85ed361dcdb51a39ff18888f4ae356
the-prism <t.castonguay.gagnon@gmail.com>
2017-11-25 11:32:45 -0500

    Physical repository

--- FitnessHabitsAndroid/app/src/main/java/com/strudelauxpommes/fitnesshabits/data/dao/PhysicalDataDAO.java
+++ FitnessHabitsAndroid/app/src/main/java/com/strudelauxpommes/fitnesshabits/data/dao/PhysicalDataDAO.java
@@ -2,9 +2,13 @@ package com.strudelauxpommes.fitnesshabits.data.dao;
 
 import android.arch.lifecycle.LiveData;
 import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
 import android.arch.persistence.room.Query;
 
 import com.strudelauxpommes.fitnesshabits.data.model.PhysicalData;
+import com.strudelauxpommes.fitnesshabits.data.model.record.PhysicalCategory;
+import com.strudelauxpommes.fitnesshabits.data.model.record.PhysicalEntry;
 
 import java.util.List;
 
@@ -14,8 +18,14 @@ import java.util.List;
 
 @Dao
 public interface PhysicalDataDAO {
-    @Query("select * " +
+    @Query("select PhysicalCategory.id, PhysicalCategory.categoryName, PhysicalCategory.intensity, PhysicalCategory.isFavorite " +
             "from PhysicalCategory " +
-            "left join (select * from PhysicalEntry where PhysicalEntry.date = 'NOW') as CategoryData on PhysicalCategory.id = CategoryData.categoryId ")
+            "left join (select PhysicalEntry.duration from PhysicalEntry where PhysicalEntry.date = 'NOW') as CategoryData on PhysicalCategory.id = CategoryData.categoryId ")
     LiveData<List<PhysicalData>> getToday();
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insetOrReplacePhysicalEntry(PhysicalEntry entry);
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insetOrReplacePhysicalCategory(PhysicalCategory category);
 }
--- FitnessHabitsAndroid/app/src/main/java/com/strudelauxpommes/fitnesshabits/data/repository/PhysicalRepository.java
+++ FitnessHabitsAndroid/app/src/main/java/com/strudelauxpommes/fitnesshabits/data/repository/PhysicalRepository.java
@@ -1,10 +1,18 @@
 package com.strudelauxpommes.fitnesshabits.data.repository;
 
+import android.annotation.SuppressLint;
 import android.arch.lifecycle.LiveData;
+import android.os.AsyncTask;
+import android.support.annotation.MainThread;
+import android.support.annotation.NonNull;
 
+import com.strudelauxpommes.fitnesshabits.data.DatabaseResource;
 import com.strudelauxpommes.fitnesshabits.data.dao.PhysicalDataDAO;
 import com.strudelauxpommes.fitnesshabits.data.model.PhysicalData;
+import com.strudelauxpommes.fitnesshabits.data.model.record.PhysicalCategory;
+import com.strudelauxpommes.fitnesshabits.data.model.record.PhysicalEntry;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -22,7 +30,40 @@ public class PhysicalRepository {
     public LiveData<List<PhysicalData>> loadDailyData() {
         if (listPhysicalLiveData == null) {
             PhysicalData defaultData = new PhysicalData(1,"Mock",5,2, false);
+            List<PhysicalData> list = new ArrayList<>();
+            list.add(defaultData);
+            listPhysicalLiveData = new DatabaseResource<List<PhysicalData>>(list){
+                @NonNull
+                @Override
+                protected LiveData<List<PhysicalData>> loadFromDb() {
+                    return physicalDataDAO.getToday();
+                }
+            }.getAsLiveData();;
         }
         return null;
+    }
+
+    @SuppressLint("StaticFieldLeak")
+    @MainThread
+    public void savePhysicalEntry(PhysicalEntry entry) {
+        new AsyncTask<Void,Void,Void>() {
+            @Override
+            protected Void doInBackground(Void... voids) {
+                physicalDataDAO.insetOrReplacePhysicalEntry(entry);
+                return null;
+            }
+        }.execute();
+    }
+
+    @SuppressLint("StaticFieldLeak")
+    @MainThread
+    public void savePhysicalCategory(PhysicalCategory category) {
+        new AsyncTask<Void,Void,Void>() {
+            @Override
+            protected Void doInBackground(Void... voids) {
+                physicalDataDAO.insetOrReplacePhysicalCategory(category);
+                return null;
+            }
+        }.execute();
     }
 }
