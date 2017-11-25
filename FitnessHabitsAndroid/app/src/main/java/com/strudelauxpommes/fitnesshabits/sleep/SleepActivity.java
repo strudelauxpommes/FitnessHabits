8a44a37971f5dfe3386d68ffa5042ec85f2e2fab
tirvaxx <novis.mathieu@courrier.uqam.ca>
2017-11-25 13:20:49 -0500

    activity view works

--- FitnessHabitsAndroid/app/src/main/java/com/strudelauxpommes/fitnesshabits/sleep/SleepActivity.java
+++ FitnessHabitsAndroid/app/src/main/java/com/strudelauxpommes/fitnesshabits/sleep/SleepActivity.java
@@ -1,7 +1,15 @@
 package com.strudelauxpommes.fitnesshabits.sleep;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.strudelauxpommes.fitnesshabits.R;
+
 public class SleepActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sommeil);
+    }
 
 }
