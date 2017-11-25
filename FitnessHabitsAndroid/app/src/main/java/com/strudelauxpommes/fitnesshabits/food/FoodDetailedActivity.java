adef78698bed441a49fff670f3eef4ec511905ef
Frederik-HP\Frederik <frederik.auger@gmail.com>
2017-11-25 14:04:33 -0500

    Navigation entre Sommaire et Détail

--- FitnessHabitsAndroid/app/src/main/java/com/strudelauxpommes/fitnesshabits/food/FoodDetailedActivity.java
+++ FitnessHabitsAndroid/app/src/main/java/com/strudelauxpommes/fitnesshabits/food/FoodDetailedActivity.java
@@ -1,5 +1,6 @@
 package com.strudelauxpommes.fitnesshabits.food;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -11,5 +12,6 @@ public class FoodDetailedActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food_detailed);
+
     }
 }
--- FitnessHabitsAndroid/app/src/main/java/com/strudelauxpommes/fitnesshabits/food/FoodFragment.java
+++ FitnessHabitsAndroid/app/src/main/java/com/strudelauxpommes/fitnesshabits/food/FoodFragment.java
@@ -7,6 +7,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.strudelauxpommes.fitnesshabits.R;
 
@@ -20,18 +22,34 @@ public class FoodFragment extends Fragment {
         // Required empty public constructor
     }
 
-    public void openDetail(View v)
+    /*public void openDetail(View v)
     {
-        Intent intent = new Intent(v.getContext(), FoodDetailedActivity.class);
+        Intent intent = new Intent(getActivity(), FoodDetailedActivity.class);
         startActivity(intent);
-    }
+    }*/
 
+    Button header;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_food, container, false);
+
+        View rootView = inflater.inflate(R.layout.fragment_food, container, false);
+
+        header = (Button)rootView.findViewById(R.id.btnHeader);
+
+        header.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FoodFragment.this.getActivity(), FoodDetailedActivity.class);
+                startActivity(intent);
+            }
+
+        });
+
+        return rootView;
     }
+
+
 
 }
