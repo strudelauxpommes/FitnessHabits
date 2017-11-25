74004a0f00204944aa03a89734756389a8f7995b
Samuel Chamberland <sam.chamberland@me.com>
2017-11-25 11:31:53 -0500

    Add listener and instantiate Intent

--- FitnessHabitsAndroid/app/src/main/java/com/strudelauxpommes/fitnesshabits/MainActivity.java
+++ FitnessHabitsAndroid/app/src/main/java/com/strudelauxpommes/fitnesshabits/MainActivity.java
@@ -16,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -30,3 +31,4 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 }
+
