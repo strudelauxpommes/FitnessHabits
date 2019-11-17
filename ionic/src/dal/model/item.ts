b4c2f193b7b5311a672e5bed9b0df267d321e8b1
Jean Salama <jeansos3@hotmail.com>
2019-11-17 12:41:09 -0500

    model: add new types for Dal

---
+++ ionic/src/dal/model/item.ts
@@ -0,0 +1,5 @@
+export default interface Item {
+    timestampMs: number;
+    dateTime: number;
+    value: any;
+}
---
+++ ionic/src/dal/model/value.ts
@@ -0,0 +1,3 @@
+export default interface Value {
+    value: string | number | boolean | null;
+}
