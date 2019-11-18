78a3fed2ca5009b8ab2958dd8f2b41b14bc73c73
Ghenadie Mocanu <mocanu.ghenadie@uqam.ca>
2019-11-18 01:56:08 -0500

    UI export-import données

--- ionic/src/pages/Home.tsx
+++ ionic/src/pages/Home.tsx
@@ -1,26 +1,51 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import React from 'react';
+import React  from 'react';
+import { IonTabs, IonRouterOutlet, IonTabBar, IonTabButton, IonIcon, IonLabel, IonMenu, IonHeader, IonToolbar, IonTitle,
+  IonContent, IonList, IonItem
+} from '@ionic/react';
+import { Route, Redirect } from 'react-router';
+import { add, trash, download, settings, arrowRoundForward } from 'ionicons/icons';
 
-const Home: React.FC = () => {
+import Export from './export';
+import Import from './import';
+import Supression from './purge';
+
+interface MainTabsProps { }
+
+const MainTabs: React.FC<MainTabsProps> = () => {
+
   return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Ionic Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent className="ion-padding">
-        The world is your oyster.
-        <p>
-          If you get lost, the{' '}
-          <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/">
-            docs
-          </a>{' '}
-          will be your guide.
-        </p>
-      </IonContent>
-    </IonPage>
+    <IonTabs>
+      <IonRouterOutlet>
+        <Redirect exact path="/tabs" to="/tabs/home" />
+        <Route path="/tabs/export" render={() => <Export />} exact={true} />
+        <Route path="/tabs/import" render={() => <Import />} exact={true} />
+        <Route path="/tabs/purge" render={() => <Supression />} exact={true} />   
+      </IonRouterOutlet>
+      <IonTabBar slot="bottom">
+        <IonTabButton tab="ajoutDonnees" href="/tabs/addData">
+          <IonIcon icon={add} />
+          <IonLabel></IonLabel>
+        </IonTabButton>
+        <IonTabButton tab="purge" href="/tabs/purge">
+          <IonIcon icon={trash} />
+          <IonLabel></IonLabel>
+        </IonTabButton>
+        <IonTabButton tab="export" href="/tabs/export">
+          <IonIcon icon={download} />
+          <IonLabel></IonLabel>
+        </IonTabButton>
+        <IonTabButton tab="import" href="/tabs/import">
+          <IonIcon icon={arrowRoundForward} />
+          <IonLabel></IonLabel>
+        </IonTabButton>
+        <IonTabButton tab="settings" href="/tabs/settings">
+          <IonIcon icon={settings} />
+          <IonLabel></IonLabel>
+        </IonTabButton>
+      </IonTabBar>
+    </IonTabs>
+    
   );
 };
 
-export default Home;
+export default MainTabs;
