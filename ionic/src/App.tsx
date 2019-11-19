a62829ffbad1a93253326576dfded224de1f2234
Philippe Gerbeau <philippe.gerbeau.93@gmail.com>
2019-11-19 12:40:20 -0500

    Add ActivityDetails page and routing

--- ionic/src/App.tsx
+++ ionic/src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Redirect, Route } from 'react-router-dom';
 import { IonApp, IonRouterOutlet } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import Home from './pages/Home';
+import ActivityDetails from './pages/ActivityDetails';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -27,7 +27,7 @@ const App: React.FC = () => (
   <IonApp>
     <IonReactRouter>
       <IonRouterOutlet>
-        <Route path="/home" component={Home} exact={true} />
+        <Route path="/home" component={ActivityDetails} exact={true} />
         <Route exact path="/" render={() => <Redirect to="/home" />} />
       </IonRouterOutlet>
     </IonReactRouter>
