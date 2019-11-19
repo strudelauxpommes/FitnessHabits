6edd7e0c402d2187186002611147dcf8b2067600
Guillaume Levesque <guibere@icloud.com>
2019-11-18 21:48:56 -0500

    Prototype done
    FInalized prototype

--- ionic/src/pages/Home.tsx
+++ ionic/src/pages/Home.tsx
@@ -1,9 +1,19 @@
 import React  from 'react';
 import { IonTabs, IonRouterOutlet, IonTabBar, IonTabButton, IonIcon, IonLabel, IonMenu, IonHeader, IonToolbar, IonTitle,
-  IonContent, IonList, IonItem, IonPage, IonAvatar, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonButton
+  IonContent, IonList, IonItem, IonPage, IonAvatar, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonButton, IonCardSubtitle, IonListHeader,
+  IonNote,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonInput,
+  IonSelect,
+  IonSelectOption,
+  IonCheckbox,
+  IonRadio,
+  IonRadioGroup
 } from '@ionic/react';
 import { Route, Redirect } from 'react-router';
-import { add, trash, download, settings, arrowRoundForward, calendar } from 'ionicons/icons';
+import { add, trash, download, settings, arrowRoundForward, calendar, locate } from 'ionicons/icons';
 
 import Export from './export';
 import Import from './import';
@@ -52,8 +62,43 @@ const Home: React.FC = () => {
           </IonCardHeader>
 
           <IonCardContent>
-            Keep close to Nature's heart... and break clear away, once in awhile, and
-            climb a mountain or spend a week in the woods. Wash your spirit clean.
+              <IonGrid>
+                <IonRow>
+                  <IonCol>Activité</IonCol>
+                  <IonCol>Durée</IonCol>
+                  <IonCol>Intensité</IonCol>
+                </IonRow>
+
+                <IonRow>
+                  <IonCol>Karaté</IonCol>
+                  <IonCol>1:17</IonCol>
+                  <IonCol>M</IonCol>
+                </IonRow>
+
+                <IonRow>
+                  <IonCol>Jogging</IonCol>
+                  <IonCol>hh:mm</IonCol>
+                  <IonCol>H</IonCol>
+                </IonRow>
+
+                <IonRow>
+                  <IonCol>HIIT</IonCol>
+                  <IonCol>00:32</IonCol>
+                  <IonCol>H</IonCol>
+                </IonRow>
+              </IonGrid>
+
+              <IonHeader>Autre :</IonHeader>
+              <IonRow>
+                  <IonInput placeholder="Nom de l'activité" color="dark"></IonInput>
+                  <IonInput placeholder="Durée" color="dark"> </IonInput>
+                  <IonSelect placeholder="Intensité" interface="popover">
+                    <IonSelectOption>F</IonSelectOption>
+                    <IonSelectOption>M</IonSelectOption>
+                    <IonSelectOption>H</IonSelectOption>
+                  </IonSelect>
+                  <IonButton shape="round"><IonIcon icon={add} /></IonButton>
+                </IonRow>
           </IonCardContent>
         </IonCard>
 
@@ -63,8 +108,24 @@ const Home: React.FC = () => {
           </IonCardHeader>
 
           <IonCardContent>
-            Keep close to Nature's heart... and break clear away, once in awhile, and
-            climb a mountain or spend a week in the woods. Wash your spirit clean.
+            <IonGrid>
+            <IonRow>
+              <IonCol>Matin</IonCol>
+              <IonCol><IonCheckbox></IonCheckbox></IonCol>
+            </IonRow>
+            <IonRow>
+              <IonCol>Midi</IonCol>
+              <IonCol><IonCheckbox></IonCheckbox></IonCol>
+            </IonRow>
+            <IonRow>
+              <IonCol>Souper</IonCol>
+              <IonCol><IonCheckbox></IonCheckbox></IonCol>
+            </IonRow>
+            <IonRow>
+              <IonCol>Coucher</IonCol>
+              <IonCol><IonCheckbox></IonCheckbox></IonCol>
+            </IonRow>
+            </IonGrid>
           </IonCardContent>
         </IonCard>
 
@@ -74,8 +135,29 @@ const Home: React.FC = () => {
           </IonCardHeader>
 
           <IonCardContent>
-            Keep close to Nature's heart... and break clear away, once in awhile, and
-            climb a mountain or spend a week in the woods. Wash your spirit clean.
+          <IonGrid>
+            <IonRow>
+              <IonCol>Début</IonCol>
+              <IonCol>Fin</IonCol>
+              <IonCol># de réveils</IonCol>
+            </IonRow>
+            <IonRow>
+              <IonCol><IonInput placeholder="23:00" color="dark"></IonInput></IonCol>
+              <IonCol><IonInput placeholder="06:30" color="dark"></IonInput></IonCol>
+              <IonCol><IonItem>2</IonItem></IonCol>
+            </IonRow>
+            <IonRow>
+              <IonCol><IonInput placeholder="15:00" color="dark"></IonInput></IonCol>
+              <IonCol><IonInput placeholder="16:30" color="dark"></IonInput></IonCol>
+              <IonCol><IonItem>0</IonItem></IonCol>
+            </IonRow>
+          </IonGrid>
+          <IonHeader>Ajouter un réveil :</IonHeader>
+              <IonRow>
+                  <IonInput placeholder="Heure coucher" color="dark"></IonInput>
+                  <IonInput placeholder="Heure réveil" color="dark"> </IonInput>
+                  <IonButton shape="round"><IonIcon icon={add} /></IonButton>
+                </IonRow>
           </IonCardContent>
         </IonCard>
 
@@ -85,8 +167,18 @@ const Home: React.FC = () => {
           </IonCardHeader>
 
           <IonCardContent>
-            Keep close to Nature's heart... and break clear away, once in awhile, and
-            climb a mountain or spend a week in the woods. Wash your spirit clean.
+            <IonRow>
+              <IonItem>Poids : </IonItem>
+              <IonInput placeholder="150.9" color="dark"></IonInput>
+              <IonSelect value="Lbs" interface="popover">
+                    <IonSelectOption selected>Lbs</IonSelectOption>
+                    <IonSelectOption>Kgs</IonSelectOption>
+                  </IonSelect>
+            </IonRow>
+            <IonRow>
+              <IonItem>Pourcentage de gras : </IonItem>
+              <IonInput placeholder="0.00%" color="dark"></IonInput>
+            </IonRow>
           </IonCardContent>
         </IonCard>
 
@@ -96,19 +188,82 @@ const Home: React.FC = () => {
           </IonCardHeader>
 
           <IonCardContent>
-            Keep close to Nature's heart... and break clear away, once in awhile, and
-            climb a mountain or spend a week in the woods. Wash your spirit clean.
+            <IonGrid>
+              <IonRow>
+                <IonCol></IonCol>
+                <IonCol><IonItem># Portions</IonItem></IonCol>
+                <IonCol></IonCol>
+                <IonCol></IonCol>
+              </IonRow>
+              <IonRow>
+                <IonCol>Protéines</IonCol>
+                <IonCol><IonItem>3</IonItem></IonCol>
+                <IonCol><IonButton shape="round">+1</IonButton></IonCol>
+                <IonCol><IonButton shape="round" color="secondary">-1</IonButton></IonCol>
+              </IonRow>
+              <IonRow>
+                <IonCol>Légumes</IonCol>
+                <IonCol><IonItem>2</IonItem></IonCol>
+                <IonCol><IonButton shape="round">+1</IonButton></IonCol>
+                <IonCol><IonButton shape="round" color="secondary">-1</IonButton></IonCol>
+              </IonRow>
+              <IonRow>
+                <IonCol>Glucides</IonCol>
+                <IonCol><IonItem>3</IonItem></IonCol>
+                <IonCol><IonButton shape="round">+1</IonButton></IonCol>
+                <IonCol><IonButton shape="round" color="secondary">-1</IonButton></IonCol>
+              </IonRow>
+              <IonRow>
+                <IonCol>Lipides</IonCol>
+                <IonCol><IonItem>1</IonItem></IonCol>
+                <IonCol><IonButton shape="round">+1</IonButton></IonCol>
+                <IonCol><IonButton shape="round" color="secondary">-1</IonButton></IonCol>
+              </IonRow>
+              <IonRow>
+                <IonCol>Fruits</IonCol>
+                <IonCol><IonItem>5</IonItem></IonCol>
+                <IonCol><IonButton shape="round">+1</IonButton></IonCol>
+                <IonCol><IonButton shape="round" color="secondary">-1</IonButton></IonCol>
+              </IonRow>
+            </IonGrid>
           </IonCardContent>
         </IonCard>
 
         <IonCard>
           <IonCardHeader>
             <IonCardTitle>Breuvage</IonCardTitle>
+            <IonCardSubtitle>1500 ml</IonCardSubtitle>
           </IonCardHeader>
 
           <IonCardContent>
-            Keep close to Nature's heart... and break clear away, once in awhile, and
-            climb a mountain or spend a week in the woods. Wash your spirit clean.
+           <IonGrid>
+             <IonRow>
+               <IonCol><IonItem>Type</IonItem></IonCol>
+               <IonCol><IonItem>Volume</IonItem></IonCol>
+               <IonCol><IonItem>Quantité</IonItem></IonCol>
+             </IonRow>
+             <IonRow>
+               <IonCol><IonItem>Eau</IonItem></IonCol>
+               <IonCol><IonItem>250 ml</IonItem></IonCol>
+               <IonCol><IonItem>6</IonItem></IonCol>
+             </IonRow>
+             <IonRow>
+               <IonCol><IonItem>Café</IonItem></IonCol>
+               <IonCol><IonItem>250 ml</IonItem></IonCol>
+               <IonCol><IonItem>1</IonItem></IonCol>
+             </IonRow>
+             <IonRow>
+               <IonCol><IonItem>Tisane</IonItem></IonCol>
+               <IonCol><IonItem>250 ml</IonItem></IonCol>
+               <IonCol><IonItem>2</IonItem></IonCol>
+             </IonRow>
+           </IonGrid>
+          <IonCol><IonItem>Autre : </IonItem></IonCol>
+            <IonRow>
+             <IonCol><IonInput placeholder="Type" color="dark"></IonInput></IonCol>
+             <IonCol><IonInput placeholder="Volume" color="dark"></IonInput></IonCol>
+             <IonCol><IonButton shape="round"><IonIcon icon={add} /></IonButton></IonCol>
+           </IonRow>
           </IonCardContent>
         </IonCard>
 
@@ -118,27 +273,49 @@ const Home: React.FC = () => {
           </IonCardHeader>
 
           <IonCardContent>
-            Keep close to Nature's heart... and break clear away, once in awhile, and
-            climb a mountain or spend a week in the woods. Wash your spirit clean.
+           <IonGrid>
+             <IonRow>
+               <IonCol><IonItem>Type</IonItem></IonCol>
+               <IonCol><IonItem>Volume</IonItem></IonCol>
+               <IonCol><IonItem>Quantité</IonItem></IonCol>
+             </IonRow>
+             <IonRow>
+               <IonCol><IonItem>Vin</IonItem></IonCol>
+               <IonCol><IonItem>250 ml</IonItem></IonCol>
+               <IonCol><IonItem>6</IonItem></IonCol>
+             </IonRow>
+             <IonRow>
+               <IonCol><IonItem>Vodka</IonItem></IonCol>
+               <IonCol><IonItem>1 oz</IonItem></IonCol>
+               <IonCol><IonItem>1</IonItem></IonCol>
+             </IonRow>
+             <IonRow>
+               <IonCol><IonItem>Jaggermeister</IonItem></IonCol>
+               <IonCol><IonItem>2 oz</IonItem></IonCol>
+               <IonCol><IonItem>2</IonItem></IonCol>
+             </IonRow>
+           </IonGrid>
+          <IonCol><IonItem>Autre : </IonItem></IonCol>
+            <IonRow>
+             <IonCol><IonInput placeholder="Type" color="dark"></IonInput></IonCol>
+             <IonCol><IonInput placeholder="Volume" color="dark"></IonInput></IonCol>
+             <IonCol><IonSelect value="ml" interface="popover">
+                    <IonSelectOption selected>ml</IonSelectOption>
+                    <IonSelectOption>oz</IonSelectOption>
+                  </IonSelect></IonCol>
+             <IonCol><IonButton shape="round"><IonIcon icon={add} /></IonButton></IonCol>
+           </IonRow>
           </IonCardContent>
         </IonCard>
       </IonContent>
 
       <IonTabBar slot="bottom">
-        <IonTabButton tab="ajoutDonnees" href="/">
-          <IonIcon icon={add} />
-          <IonLabel></IonLabel>
-        </IonTabButton>
-        <IonTabButton tab="purge" href="/">
-          <IonIcon icon={trash} />
+        <IonTabButton tab="objectifs" href="/">
+          <IonIcon icon={locate} />
           <IonLabel></IonLabel>
         </IonTabButton>
         <IonTabButton tab="export" href="/">
           <IonIcon icon={download} />
-          <IonLabel></IonLabel>
-        </IonTabButton>
-        <IonTabButton tab="import" href="/">
-          <IonIcon icon={arrowRoundForward} />
           <IonLabel></IonLabel>
         </IonTabButton>
         <IonTabButton tab="settings" href="/">
