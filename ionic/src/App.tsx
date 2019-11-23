99ecc485542d37f245137d34b121f1175fac0f9e
Guillaume Levesque <guillaume.levesque@code3.ca>
2019-11-23 13:38:43 -0500

    Attempt to fix merge with Beverage

--- ionic/src/App.tsx
+++ ionic/src/App.tsx
@@ -1,46 +1,51 @@
-import { IonApp, IonRouterOutlet } from '@ionic/react';
-import { IonReactRouter } from '@ionic/react-router';
+import { IonApp, IonRouterOutlet } from "@ionic/react";
+import { IonReactRouter } from "@ionic/react-router";
 /* Core CSS required for Ionic components to work properly */
-import '@ionic/react/css/core.css';
-import '@ionic/react/css/display.css';
-import '@ionic/react/css/flex-utils.css';
-import '@ionic/react/css/float-elements.css';
+import "@ionic/react/css/core.css";
+import "@ionic/react/css/display.css";
+import "@ionic/react/css/flex-utils.css";
+import "@ionic/react/css/float-elements.css";
 /* Basic CSS for apps built with Ionic */
-import '@ionic/react/css/normalize.css';
+import "@ionic/react/css/normalize.css";
 /* Optional CSS utils that can be commented out */
-import '@ionic/react/css/padding.css';
-import '@ionic/react/css/structure.css';
-import '@ionic/react/css/text-alignment.css';
-import '@ionic/react/css/text-transformation.css';
-import '@ionic/react/css/typography.css';
+import "@ionic/react/css/padding.css";
+import "@ionic/react/css/structure.css";
+import "@ionic/react/css/text-alignment.css";
+import "@ionic/react/css/text-transformation.css";
+import "@ionic/react/css/typography.css";
 import moment from "moment";
 import { default as React } from "react";
 import { Redirect, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import FoodAdd from "./pages/nourriture/FoodAdd";
 import FoodDailyIntake from "./pages/nourriture/FoodDailyIntake";
-import FoodList from './pages/nourriture/FoodList';
+import FoodList from "./pages/nourriture/FoodList";
 /* Food import */
 import FoodSummary from "./pages/nourriture/FoodSummary";
-import { AddSleepLineForm } from './pages/sleep/AddSleepLineForm';
+import { AddSleepLineForm } from "./pages/sleep/AddSleepLineForm";
 /* Sleep Imports */
-import SleepDetail from './pages/sleep/SleepDetail';
+import SleepDetail from "./pages/sleep/SleepDetail";
 import SleepSummary from "./pages/sleep/SleepSummary";
-import { init } from './services/i18n/i18n';
-import './theme/sleep.css';
+import { init } from "./services/i18n/i18n";
+import "./theme/sleep.css";
+/* Beverage import */
+import BeveragesSummary from "./pages/Beverages/BeveragesSummary";
+import BeveragesDetail from "./pages/Beverages/BeveragesDetail";
 /* Theme variables */
-import './theme/variables.css';
+import "./theme/variables.css";
 
 /* Temporary activeDate global variable */
 export const activeDate = moment();
 
 // Init language
-init()
+init();
 
 const App: React.FC = () => (
   <IonApp>
     <IonReactRouter>
       <IonRouterOutlet>
+        <Route path="/beverages-summary" component={BeveragesSummary} />
+        <Route path="/beverages-detail" component={BeveragesDetail} />
         <Route path="/home" component={Home} exact={true} />
         <Route exact path="/" render={() => <Redirect to="/home" />} />
         <Route path="/food-summary" component={FoodSummary} />
