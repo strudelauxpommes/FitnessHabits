4c006b8f2255ffaaabc4a708f063d0e413e3916b
MFarradji <farradjimelissa@gmail.com>
2019-11-23 10:44:09 -0500

    food-add interface completer v 1.0

--- ionic/src/pages/nourriture/FoodAdd.tsx
+++ ionic/src/pages/nourriture/FoodAdd.tsx
@@ -1,19 +1,94 @@
 import { Component } from 'react';
 import React from 'react';
-import { IonApp, IonHeader, IonContent, IonText } from '@ionic/react';
+import { IonApp, IonHeader, IonContent, IonText, IonPage, IonToolbar, IonGrid, IonRow, IonCol, IonTitle, IonItem, IonLabel, IonInput, IonCardHeader, IonCard, IonCardContent, IonIcon, IonButton } from '@ionic/react';
+import { MDBIcon } from 'mdbreact';
+import { moon, remove, add, checkmark } from 'ionicons/icons';
 
 export default class FoodAdd  extends Component {
 
     render() {
         return (
-          <IonApp>
-            <IonHeader>
-                <IonText> FoodAdd </IonText>
-            </IonHeader>
-            <IonContent>
-      
-            </IonContent>
-          </IonApp>
+          <IonPage>
+      <IonHeader className="background-black">
+        <IonToolbar>
+          <IonGrid>
+            <IonRow>
+              <IonCol size="4.5">
+                <MDBIcon size="1x" icon="backward" />
+              </IonCol>
+              <IonCol> Nutrition </IonCol>
+            </IonRow>
+          </IonGrid>
+        </IonToolbar>
+      </IonHeader>
+
+
+      <IonContent className="ion-padding">
+        <IonCard>
+          <IonCardHeader>
+          <IonToolbar>
+          <IonTitle>DINER</IonTitle>
+        </IonToolbar>
+          </IonCardHeader>
+        <IonCardContent>
+        <IonGrid>
+          <IonRow>
+            <IonItem>
+              <IonLabel>Nom : </IonLabel>
+              <IonInput value="custom"></IonInput>
+            </IonItem>
+          </IonRow>
+          <IonRow>
+            <IonItem>
+              <IonLabel>Taille de la portion : </IonLabel>
+              <IonInput type="number"></IonInput>
+              <IonLabel>g </IonLabel>
+            </IonItem>
+          </IonRow>
+          <IonRow>
+            <IonItem>
+              <IonLabel>Quantite de la portion : </IonLabel>
+              <IonInput type="number"></IonInput>
+            </IonItem>
+          </IonRow>
+          <IonRow>
+            <IonItem>
+              <IonLabel>Quantite de glucide : </IonLabel>
+              <IonInput type="number"></IonInput>
+              <IonLabel>g </IonLabel>
+            </IonItem>
+          </IonRow>
+          <IonRow>
+            <IonItem>
+              <IonLabel>Quantite de proteine : </IonLabel>
+              <IonInput type="number"></IonInput>
+              <IonLabel>g </IonLabel>
+            </IonItem>
+          </IonRow>
+          <IonRow>
+            <IonItem>
+              <IonLabel>Quantite de glucide : </IonLabel>
+              <IonInput type="number"></IonInput>
+              <IonLabel>g </IonLabel>
+            </IonItem>
+          </IonRow>
+        </IonGrid>
+
+        <IonGrid>
+         <IonRow>
+            <IonCol> </IonCol>
+            <IonCol >
+             <IonButton fill="solid" color="dark" expand="block">
+             <IonIcon icon={checkmark}></IonIcon>
+              </IonButton>
+            </IonCol>
+            <IonCol> </IonCol>
+          </IonRow>
+        </IonGrid>
+        </IonCardContent>
+        </IonCard>
+      </IonContent>
+    </IonPage>
         );
       }
 
