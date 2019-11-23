42ee57c37efb92ea95614afda19e02d985349828
Dr_Ground <francis_grondin@hotmail.com>
2019-11-23 15:21:50 -0500

    beautify

--- ionic/src/dal/dateWrapper/dateWrapperImpl.test.ts
+++ ionic/src/dal/dateWrapper/dateWrapperImpl.test.ts
@@ -1,29 +1,29 @@
-import { DateWrapperImpl } from './dateWrapperImpl';
 import DateWrapper from './dateWrapper';
+import { DateWrapperImpl } from './dateWrapperImpl';
 
-describe('DateWrapper', function() {
-    let dateWrapper: DateWrapper ;
+describe('DateWrapper', function () {
+    let dateWrapper: DateWrapper;
 
-    describe('constructor', function() {
-        it('should have a timestamp lesser or equal to Date.now()', function() {
+    describe('constructor', function () {
+        it('should have a timestamp lesser or equal to Date.now()', function () {
             dateWrapper = new DateWrapperImpl(new Date());
             const actual = dateWrapper.getTimestampMs();
             expect(actual).toBeLessThanOrEqual(Date.now());
         });
 
-        it('should have the same date (year, month and day)', function() {
+        it('should have the same date (year, month and day)', function () {
             const actual = new DateWrapperImpl(new Date("11/2/2019, 3:33:50 PM"));
             expect(actual.isSameDate(new Date("11/2/2019, 5:33:50 AM"))).toBe(true);
         });
 
-        it('should return if it is at the begin of the dates range', function() {
+        it('should return if it is at the begin of the dates range', function () {
             const begin = new Date("2019-01-01");
             const end = new Date("2019-01-03");
             const actual = new DateWrapperImpl(begin);
             expect(actual.isBetweenDates(begin, end)).toBe(true);
         });
 
-        it('should return if it is between two date', function() {
+        it('should return if it is between two date', function () {
             const begin = new Date("2019-01-01");
             const middle = new Date("2019-01-02");
             const end = new Date("2019-01-03");
@@ -31,14 +31,14 @@ describe('DateWrapper', function() {
             expect(actual.isBetweenDates(begin, end)).toBe(true);
         });
 
-        it('should return if it is at the end of the dates range', function() {
+        it('should return if it is at the end of the dates range', function () {
             const begin = new Date("2019-01-01");
             const end = new Date("2019-01-03");
             const actual = new DateWrapperImpl(end);
             expect(actual.isBetweenDates(begin, end)).toBe(true);
         });
 
-        it('should return false if it is out ouf range', function() {
+        it('should return false if it is out ouf range', function () {
             const begin = new Date("2019-01-01");
             const end = new Date("2019-01-03");
             const afterEnd = new Date("2019-01-04");
