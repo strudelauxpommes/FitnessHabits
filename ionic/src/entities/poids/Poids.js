764f491252ff7065b95173248d828eaf2f74e38c
unknown <faridbedrane@gmail.com>
2019-11-23 10:35:38 -0500

    Ajout de poids avec un interface graphique améliorer.

---
+++ ionic/src/entities/poids/Poids.js
@@ -0,0 +1,94 @@
+import {
+    IonCard,
+    IonCardContent,
+    IonTitle,
+    IonInput,
+    IonLabel,
+    IonItem,
+    IonGrid,
+	IonIcon,
+    IonRow,
+    IonCol
+} from '@ionic/react';
+  import React, { Component , ChangeEvent} from 'react';
+  import { speedometer} from 'ionicons/icons';
+  import '../../theme/poids.css';;
+  //import Data from '../dal/Dal';
+  class Poids extends Component {
+    state = {
+        poids: 0,
+        taille: 180,
+        imc: 0
+    };
+    surpoids = (event) => {
+        var input = document.getElementById('poids');
+        var imcslot = document.getElementById('imc');
+        var timeout = null;
+        var imc = 0;
+        var taille = (this.state.taille)/100;
+        input.onkeyup = (e) => {
+            clearTimeout(timeout);
+            timeout = setTimeout( ()=> {
+                //input = document.getElementById('poids');
+                this.setState({poids: input.value});
+                if(this.state.poids != 0){
+					imc = Math.round((this.state.poids/(taille*taille)) * 100)/100;
+					this.setState({imc: imc});
+					if(imc <= 18.49){
+						imcslot.innerHTML = '<IonTitle id="imc">&#x1F7E1 IMC:' +imc+'</IonTitle>';
+					}else if(imc >= 18.50 && imc <= 24.99){
+						imcslot.innerHTML = '<IonTitle id="imc">&#x26AA IMC:' +imc+'</IonTitle>';
+					}else if(imc >= 25 && imc <= 29.99){
+						imcslot.innerHTML = '<IonTitle id="imc">&#x26A0	IMC:' +imc+'</IonTitle>';
+					}else if(imc >= 30 && imc <= 34.99){
+						imcslot.innerHTML = '<IonTitle id="imc">&#x1F534 IMC:' +imc+'</IonTitle>';
+					}else if(imc >= 35 && imc <= 39.99){
+						imcslot.innerHTML = '<IonTitle id="imc">&#x1F6D1 IMC:' +imc+'</IonTitle>';
+					}else{
+						imcslot.innerHTML = '<IonTitle id="imc">&#x1F6A8 IMC:' +imc+'</IonTitle>';
+					}    	
+			}else{
+				imcslot.innerHTML = '<IonTitle id="imc"></IonTitle>';
+			}
+                console.log(this.state.poids);
+                console.log(imc);
+            }, 500);
+        }
+    }
+    render() {
+        return (
+            <IonCard>
+                <IonGrid class="Wstyle">
+                    <IonRow>
+                        <IonCol>
+                            <IonTitle>
+							<IonIcon icon={speedometer} /> Poids</IonTitle>
+                        </IonCol>
+                        <IonCol>
+							<IonTitle id="imc"></IonTitle>
+                        </IonCol>
+                    </IonRow>
+                </IonGrid>
+                 <IonCardContent>
+                    <IonItem text-center="text-center">
+                        <IonInput type="number" id="poids" pattern="[0-9]*" 
+						ng-model="vm.onlyNumbers" stacked="stacked" required 
+						onIonChange={this.surpoids}></IonInput>
+						<IonLabel>
+                        Kg</IonLabel>
+                    </IonItem>
+				</IonCardContent>
+				<IonCardContent>
+                    <IonItem text-center="text-center">
+                        <IonInput type="number" pattern="^[0-9]{2-4}" maxlength={4}
+						ng-model="vm.onlyNumbers" stacked="stacked" placeholder="--%"></IonInput>
+						<IonLabel >
+                         % Gras</IonLabel>
+                    </IonItem>
+                </IonCardContent>
+            </IonCard>
+            
+        );
+    }
+}
+export default Poids;
--- ionic/src/pages/Home.tsx
+++ ionic/src/pages/Home.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 import { Redirect, Route } from 'react-router';
 import Export from './export';
 import Import from './import';
+import Poids from '../entities/poids/Poids';
 import Supression from './purge';
 import T from 'i18n-react';
 
@@ -27,7 +28,7 @@ const Home: React.FC = () => {
       <IonHeader>
         <IonToolbar>
           <IonTitle>
-           {T.translate("app.title")}
+            {T.translate("app.title")}
           </IonTitle>
         </IonToolbar>
       </IonHeader>
@@ -38,11 +39,11 @@ const Home: React.FC = () => {
             <img src="https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=retro&f=y%22" />
           </IonAvatar>
           <IonLabel>
-            <h1>Username</h1>  
+            <h1>Username</h1>
           </IonLabel>
           <IonButton color="light" slot="end">
-              <IonIcon slot="start" icon={calendar} />
-              11 Novembre 2019
+            <IonIcon slot="start" icon={calendar} />
+            11 Novembre 2019
           </IonButton>
         </IonItem>
 
@@ -153,27 +154,7 @@ const Home: React.FC = () => {
           </IonCardContent>
         </IonCard>
 
-        <IonCard>
-          <IonCardHeader>
-            <IonCardTitle>Poids</IonCardTitle>
-          </IonCardHeader>
-
-          <IonCardContent>
-            <IonRow>
-              <IonItem>Poids : </IonItem>
-              <IonInput placeholder="150.9" color="dark"></IonInput>
-              <IonSelect value="Lbs" interface="popover">
-                <IonSelectOption selected>Lbs</IonSelectOption>
-                <IonSelectOption>Kgs</IonSelectOption>
-              </IonSelect>
-            </IonRow>
-            <IonRow>
-              <IonItem>Pourcentage de gras : </IonItem>
-              <IonInput placeholder="0.00%" color="dark"></IonInput>
-            </IonRow>
-          </IonCardContent>
-        </IonCard>
-
+        <Poids />
         <IonCard>
           <IonCardHeader>
             <IonCardTitle>Nourriture</IonCardTitle>
