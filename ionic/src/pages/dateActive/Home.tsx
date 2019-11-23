9695809a5e3bed0b9e8deff4434bc11d6091f9ae
Samuel <castilloux.samuel@courrier.uqam.ca>
2019-11-23 10:07:47 -0500

    ajout prototype date

---
+++ ionic/src/pages/dateActive/Home.tsx
@@ -0,0 +1,93 @@
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonDatetime,
+  IonButton,
+  IonIcon
+} from '@ionic/react';
+import {calendar} from 'ionicons/icons';
+import './Home.css';
+import { SaveDateActive } from './StorageDate'; //GetDateActive
+import React from 'react';
+
+
+
+var myDate = new Date();
+const cleDate = "settings/dateActive";
+
+
+
+function ParamDate(haveLimit : boolean) {
+
+ 
+
+
+    var myDate = new Date();
+    var jour = myDate.getDate();
+    var mois = myDate.getMonth();
+    var annee = myDate.getFullYear();
+
+    var returnDate;
+
+    mois = mois + 1;
+
+    if (haveLimit) {
+        if (mois <= 3) {
+            mois = mois + 12;
+            annee = annee - 1;
+        }
+        mois = mois - 3;
+       
+    }
+
+    returnDate = annee + '-';
+
+    if (mois < 10) {
+        returnDate = returnDate + '0';
+    }
+
+    returnDate = returnDate + mois + '-';
+
+    if (jour < 10) {
+        returnDate = returnDate + '0';
+    }
+
+    
+    return returnDate + jour;
+
+
+}
+
+
+
+const Home: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar class="new-background-color">
+
+          <IonTitle color="light">FitnessHabits</IonTitle>
+          <IonButton class="new-background-color" slot="end" >
+            <IonIcon icon={calendar}></IonIcon>
+                      <IonDatetime displayFormat="YYYY/MM/DD" min={ParamDate(true)} max={ParamDate(false)} value={myDate.toString()}
+                      onIonChange={ 
+                        (e) =>   SaveDateActive(cleDate, (e.target as HTMLInputElement).value)
+                           }
+                      >
+                      </IonDatetime>
+                      
+                  </IonButton>
+
+        </IonToolbar>
+      </IonHeader>
+          <IonContent>
+        
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Home;
