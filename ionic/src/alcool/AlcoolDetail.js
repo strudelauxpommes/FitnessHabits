17b1554635dee6b6192196e4739033b1ba8539b9
loupoul <lbpmartel@gmail.com>
2019-11-23 10:48:34 -0500

    Bouton Ajout sans fonctionnement

--- ionic/src/alcool/AlcoolDetail.js
+++ ionic/src/alcool/AlcoolDetail.js
@@ -1,6 +1,10 @@
 import {
+    IonFab,
+    IonFabButton,
+    IonIcon,
     IonPage,
 } from '@ionic/react';
+import { add } from 'ionicons/icons';
 import React from 'react';
 import './Style/AlcoolDetail.scss';
 import AlcoolDetailHeader from './Components/AlcoolDetailHeader';
@@ -10,7 +14,13 @@ class AlcoolDetail extends React.Component {
         return (
             <IonPage>
                 <AlcoolDetailHeader />
-            </IonPage>);
+                <IonFab vertical="bottom" horizontal="center" slot="fixed">
+                    <IonFabButton color="alcool">
+                        <IonIcon icon={add} />
+                    </IonFabButton>
+                </IonFab>
+            </IonPage>
+        );
     }
 }
 export default AlcoolDetail;
