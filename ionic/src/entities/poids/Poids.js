6092bcc1d960c86e5ac329aec4b820792a5140ee
Youssef <benelkaid.youssef@gmail.com>
2019-11-23 14:32:17 -0500

    ajout de persistence

--- ionic/src/entities/poids/Poids.js
+++ ionic/src/entities/poids/Poids.js
@@ -6,48 +6,97 @@ import {
     IonLabel,
     IonItem,
     IonGrid,
-	IonIcon,
+    IonIcon,
     IonRow,
     IonCol
 } from '@ionic/react';
-  import React, { Component , ChangeEvent} from 'react';
-  import { speedometer} from 'ionicons/icons';
-  import '../../theme/poids.css';;
-  class Poids extends Component {
-    state = {
-        poids: 0,
-        taille: 180,
-        imc: 0
-    };
+import React, {Component, ChangeEvent} from 'react';
+import {speedometer} from 'ionicons/icons';
+import '../../theme/poids.css';
+import {DalImpl} from '../../dal/DalImpl'
+
+class Poids extends Component {
+    constructor(props) {
+        super(props);
+        this.persist = new DalImpl()
+        this.state = {
+            poids: 0,
+            taille: 179,
+            imc: 0,
+            unitePoids: null,
+            uniteTaille: null
+        };
+    }
+
+    apicall = () => {
+        if (this.persist.getLastValue("profil/poids") != null) {
+            this.setState({
+                poids: this
+                    .persist
+                    .getLastValue("profil/poids")
+            });
+        }
+
+        if (this.persist.getValue("preferences/unitePoids") != null) {
+            this.setState({
+                unitePoids: this
+                    .persist
+                    .getValue("preferences/unitePoids")
+            });
+        } else if (this.persist.getValue("preferences/unitePoids") === null) {
+            this.setState({unitePoids: "kg"});
+        }
+        if (this.persist.getValue("preferences/uniteTaille") != null) {
+            this.setState({
+                unitePoids: this
+                    .persist
+                    .getValue("preferences/uniteTaille")
+            });
+        } else if (this.persist.getValue("preferences/uniteTaille") === null) {
+            this.setState({unitePoids: "cm"});
+        }
+
+        if (this.persist.getLastValue("profil/taille") != null) {
+            this.setState({
+                poids: this
+                    .persist
+                    .getLastValue("profil/taille")
+            });
+        }
+
+    }
     surpoids = (event) => {
+
+        this.apicall();
         var input = document.getElementById('poids');
         var imcslot = document.getElementById('imc');
         var timeout = null;
         var imc = 0;
-        var taille = (this.state.taille)/100;
+        var taille = (this.state.taille) / 100;
         input.onkeyup = (e) => {
             clearTimeout(timeout);
-            timeout = setTimeout( ()=> {
+            timeout = setTimeout(() => {
                 this.setState({poids: input.value});
-                if(this.state.poids != 0){
-					imc = Math.round((this.state.poids/(taille*taille)) * 100)/100;
-					this.setState({imc: imc});
-					if(imc <= 18.49){
-						imcslot.innerHTML = '<IonTitle id="imc">&#x1F7E1 IMC:' +imc+'</IonTitle>';
-					}else if(imc >= 18.50 && imc <= 24.99){
-						imcslot.innerHTML = '<IonTitle id="imc">&#x26AA IMC:' +imc+'</IonTitle>';
-					}else if(imc >= 25 && imc <= 29.99){
-						imcslot.innerHTML = '<IonTitle id="imc">&#x26A0	IMC:' +imc+'</IonTitle>';
-					}else if(imc >= 30 && imc <= 34.99){
-						imcslot.innerHTML = '<IonTitle id="imc">&#x1F534 IMC:' +imc+'</IonTitle>';
-					}else if(imc >= 35 && imc <= 39.99){
-						imcslot.innerHTML = '<IonTitle id="imc">&#x1F6D1 IMC:' +imc+'</IonTitle>';
-					}else{
-						imcslot.innerHTML = '<IonTitle id="imc">&#x1F6A8 IMC:' +imc+'</IonTitle>';
-					}    	
-			}else{
-				imcslot.innerHTML = '<IonTitle id="imc"></IonTitle>';
-			}
+                this.persist.setValue("profil/poids",JSON.stringify(this.state.poids));
+                if (this.state.poids != 0) {
+                    imc = Math.round((this.state.poids / (taille * taille)) * 100) / 100;
+                    this.setState({imc: imc});
+                    if (imc <= 18.49) {
+                        imcslot.innerHTML = '<IonTitle id="imc">&#x1F7E1 IMC:' + imc + '</IonTitle>';
+                    } else if (imc >= 18.50 && imc <= 24.99) {
+                        imcslot.innerHTML = '<IonTitle id="imc">&#x26AA IMC:' + imc + '</IonTitle>';
+                    } else if (imc >= 25 && imc <= 29.99) {
+                        imcslot.innerHTML = '<IonTitle id="imc">&#x26A0	IMC:' + imc + '</IonTitle>';
+                    } else if (imc >= 30 && imc <= 34.99) {
+                        imcslot.innerHTML = '<IonTitle id="imc">&#x1F534 IMC:' + imc + '</IonTitle>';
+                    } else if (imc >= 35 && imc <= 39.99) {
+                        imcslot.innerHTML = '<IonTitle id="imc">&#x1F6D1 IMC:' + imc + '</IonTitle>';
+                    } else {
+                        imcslot.innerHTML = '<IonTitle id="imc">&#x1F6A8 IMC:' + imc + '</IonTitle>';
+                    }
+                } else {
+                    imcslot.innerHTML = '<IonTitle id="imc"></IonTitle>';
+                }
                 console.log(this.state.poids);
                 console.log(imc);
             }, 500);
@@ -60,32 +109,43 @@ import {
                     <IonRow>
                         <IonCol>
                             <IonTitle>
-							<IonIcon icon={speedometer} /> Poids</IonTitle>
+                                <IonIcon icon={speedometer}/>
+                                Poids</IonTitle>
                         </IonCol>
                         <IonCol>
-							<IonTitle id="imc"></IonTitle>
+                            <IonTitle id="imc"></IonTitle>
                         </IonCol>
                     </IonRow>
                 </IonGrid>
-                 <IonCardContent>
+                <IonCardContent>
                     <IonItem text-center="text-center">
-                        <IonInput type="number" id="poids" pattern="[0-9]*" 
-						ng-model="vm.onlyNumbers" stacked="stacked" required 
-						onIonChange={this.surpoids}></IonInput>
-						<IonLabel>
-                        Kg</IonLabel>
+                        <IonInput
+                            type="number"
+                            id="poids"
+                            pattern="[0-9]*"
+                            ng-model="vm.onlyNumbers"
+                            stacked="stacked"
+                            required="required"
+                            onIonChange={this.surpoids}></IonInput>
+                        <IonLabel>
+                            {this.state.unitePoids}</IonLabel>
                     </IonItem>
-				</IonCardContent>
-				<IonCardContent>
+                </IonCardContent>
+                <IonCardContent>
                     <IonItem text-center="text-center">
-                        <IonInput type="number" pattern="^[0-9]{2-4}" maxlength={4}
-						ng-model="vm.onlyNumbers" stacked="stacked" placeholder="--%"></IonInput>
-						<IonLabel >
-                         % Gras</IonLabel>
+                        <IonInput
+                            type="number"
+                            pattern="^[0-9]{2-4}"
+                            maxlength={4}
+                            ng-model="vm.onlyNumbers"
+                            stacked="stacked"
+                            placeholder="--%"></IonInput>
+                        <IonLabel >
+                            % Gras</IonLabel>
                     </IonItem>
                 </IonCardContent>
             </IonCard>
-            
+
         );
     }
 }
