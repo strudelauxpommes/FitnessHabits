7982239a01a444cf983ff9d6166568bf74e7af86
loupoul <lbpmartel@gmail.com>
2019-11-23 15:46:36 -0500

    Form part 2

--- ionic/src/alcool/AlcoolDetail.js
+++ ionic/src/alcool/AlcoolDetail.js
@@ -18,8 +18,10 @@ export default class AlcoolDetail extends React.Component {
         super(props);
         this.getDateFromParameters = this.getDateFromParameters.bind(this);
         this.setShowModal = this.setShowModal.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
         this.state = {
-            showModal: false
+            showModal: false,
+            Alcools: {}
         }
     }
 
@@ -35,12 +37,26 @@ export default class AlcoolDetail extends React.Component {
         });
     }
 
+    onSubmit(name, volume) {
+        debugger;
+        if (!this.state.Alcools[`${name}`]) {
+            var newStateObj = this.state;
+            newStateObj.Alcools[`${name}`] = {};
+            newStateObj.Alcools[`${name}`].quantity = volume;
+            console.log(newStateObj);
+            debugger;
+            this.setState(newStateObj);
+        }
+    }
+
     render() {
         this.getDateFromParameters();
         return (
             <IonPage>
                 <IonModal isOpen={this.state.showModal}>
-                    <AlcoolDetailAddForm setShowModal={this.setShowModal} />
+                    <AlcoolDetailAddForm
+                        setShowModal={this.setShowModal}
+                        onSubmit={this.onSubmit} />
                 </IonModal>
                 <AlcoolDetailHeader date={this.getDateFromParameters()} />
                 <IonFab vertical="bottom" horizontal="center" slot="fixed">
