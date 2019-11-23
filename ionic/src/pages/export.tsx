714439a706f6252e94c11f974ed357e07faadb56
Guillaume Levesque <guillaume.levesque@code3.ca>
2019-11-23 15:24:38 -0500

    Added tabs to export, import and remove

--- ionic/src/pages/export.tsx
+++ ionic/src/pages/export.tsx
@@ -1,17 +1,31 @@
 import {
-  IonPage,IonSelect, IonSelectOption,
-  IonHeader, IonToolbar, IonButtons, IonMenuButton, IonTitle,
-  IonList, IonItem, IonLabel, IonIcon, IonDatetime,
-  IonButton, IonRow, IonCol, IonContent, IonRadioGroup,
-  IonRadio, IonToast
-
-} from '@ionic/react';
-import { calendar, logIn } from 'ionicons/icons';
-import React, { useState } from 'react';
-
+  IonPage,
+  IonSelect,
+  IonSelectOption,
+  IonHeader,
+  IonToolbar,
+  IonButtons,
+  IonMenuButton,
+  IonTitle,
+  IonList,
+  IonItem,
+  IonLabel,
+  IonIcon,
+  IonDatetime,
+  IonButton,
+  IonRow,
+  IonCol,
+  IonContent,
+  IonRadioGroup,
+  IonRadio,
+  IonToast
+} from "@ionic/react";
+import { calendar, logIn } from "ionicons/icons";
+import React, { useState } from "react";
+import Tabs from "./tabs";
 
 const Export: React.FC = () => {
-const [showToast1, setShowToast] = useState(false);
+  const [showToast1, setShowToast] = useState(false);
 
   return (
     <IonPage id="export-import-list" color="#b3b3b3">
@@ -20,59 +34,84 @@ const [showToast1, setShowToast] = useState(false);
           <IonButtons slot="start">
             <IonMenuButton />
           </IonButtons>
-          <IonTitle >Exporter les données</IonTitle>
+          <IonTitle>Exporter les données</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent>
-      <IonList>
-        <IonItem>
-          <IonIcon icon={calendar} slot="start"></IonIcon>
-          <IonLabel position="stacked">À partir de</IonLabel>
-          <IonDatetime displayFormat="MMM DD, YYYY" max="2056" value="2019-09-19"></IonDatetime>
-        </IonItem>
-        <IonItem>
-          <IonIcon icon={calendar} slot="start"></IonIcon>
-          <IonLabel position="stacked">Jusqu'à </IonLabel>
-          <IonDatetime displayFormat="MMM DD, YYYY" max="2056" value={new Date().toISOString()}></IonDatetime>
-        </IonItem>
-      </IonList>
+        <IonList>
+          <IonItem>
+            <IonIcon icon={calendar} slot="start"></IonIcon>
+            <IonLabel position="stacked">À partir de</IonLabel>
+            <IonDatetime
+              displayFormat="MMM DD, YYYY"
+              max="2056"
+              value="2019-09-19"
+            ></IonDatetime>
+          </IonItem>
+          <IonItem>
+            <IonIcon icon={calendar} slot="start"></IonIcon>
+            <IonLabel position="stacked">Jusqu'à </IonLabel>
+            <IonDatetime
+              displayFormat="MMM DD, YYYY"
+              max="2056"
+              value={new Date().toISOString()}
+            ></IonDatetime>
+          </IonItem>
+        </IonList>
 
-      <IonList>
-
-        <IonRow>
-          <IonCol>
-            <IonButton color="light" expand="block">Nourriture</IonButton>
-          </IonCol>
-          <IonCol>
-            <IonButton color="light" expand="block">Breuvages</IonButton>
-          </IonCol>
-        </IonRow>
-        <IonRow>
-          <IonCol>
-            <IonButton color="light" expand="block">Suppléments</IonButton>
-          </IonCol>
-          <IonCol>
-            <IonButton color="light" expand="block">Sommeil</IonButton>
-          </IonCol>
-        </IonRow>
-        <IonRow>
-          <IonCol>
-            <IonButton color="light" expand="full" >Poids</IonButton>
-          </IonCol>
-          <IonCol>
-            <IonButton expand="full" color="light" >Activités physiques</IonButton>
-          </IonCol>
-        </IonRow>
-        <IonRow>
-          <IonCol>
-            <IonButton color="light" expand="full" >Alcool</IonButton>
-          </IonCol>
-          <IonCol>
-            <IonButton expand="full" color="light" >Glycémie</IonButton>
-          </IonCol>
-        </IonRow>
-        <IonButton color="secondary" expand="block">Toutes les données</IonButton>
-      </IonList>
+        <IonList>
+          <IonRow>
+            <IonCol>
+              <IonButton color="light" expand="block">
+                Nourriture
+              </IonButton>
+            </IonCol>
+            <IonCol>
+              <IonButton color="light" expand="block">
+                Breuvages
+              </IonButton>
+            </IonCol>
+          </IonRow>
+          <IonRow>
+            <IonCol>
+              <IonButton color="light" expand="block">
+                Suppléments
+              </IonButton>
+            </IonCol>
+            <IonCol>
+              <IonButton color="light" expand="block">
+                Sommeil
+              </IonButton>
+            </IonCol>
+          </IonRow>
+          <IonRow>
+            <IonCol>
+              <IonButton color="light" expand="full">
+                Poids
+              </IonButton>
+            </IonCol>
+            <IonCol>
+              <IonButton expand="full" color="light">
+                Activités physiques
+              </IonButton>
+            </IonCol>
+          </IonRow>
+          <IonRow>
+            <IonCol>
+              <IonButton color="light" expand="full">
+                Alcool
+              </IonButton>
+            </IonCol>
+            <IonCol>
+              <IonButton expand="full" color="light">
+                Glycémie
+              </IonButton>
+            </IonCol>
+          </IonRow>
+          <IonButton color="secondary" expand="block">
+            Toutes les données
+          </IonButton>
+        </IonList>
 
         <div className="about-header"></div>
         <div className="about-info">
@@ -93,7 +132,13 @@ const [showToast1, setShowToast] = useState(false);
           </IonRadioGroup>
           <IonRow>
             <IonCol>
-              <IonButton color="light" onClick={() => setShowToast(true)} expand="block">Exporter</IonButton>
+              <IonButton
+                color="light"
+                onClick={() => setShowToast(true)}
+                expand="block"
+              >
+                Exporter
+              </IonButton>
               <IonToast
                 isOpen={showToast1}
                 position="middle"
@@ -105,6 +150,8 @@ const [showToast1, setShowToast] = useState(false);
           </IonRow>
         </IonList>
       </IonContent>
+
+      <Tabs />
     </IonPage>
   );
 };
--- ionic/src/pages/import.tsx
+++ ionic/src/pages/import.tsx
@@ -1,20 +1,32 @@
 import {
   IonPage,
-  IonHeader, IonToolbar, IonRadioGroup, IonTitle,
-  IonList, IonItem, IonLabel, IonRadio, IonSelect, IonToast,
-  IonButton, IonRow, IonCol, IonContent, IonIcon, IonSelectOption,
+  IonHeader,
+  IonToolbar,
+  IonRadioGroup,
+  IonTitle,
+  IonList,
+  IonItem,
+  IonLabel,
+  IonRadio,
+  IonSelect,
+  IonToast,
+  IonButton,
+  IonRow,
+  IonCol,
+  IonContent,
+  IonIcon,
+  IonSelectOption
+} from "@ionic/react";
+import { logIn } from "ionicons/icons";
+import React, { useState } from "react";
 
-} from '@ionic/react';
-import { logIn } from 'ionicons/icons';
-import React, { useState } from 'react';
+import Tabs from "./tabs";
 
 const Import: React.FC = () => {
   const [showToast1, setShowToast] = useState(false);
   const [showToast2, setShowToast2] = useState(false);
   return (
-
     <IonPage id="export-import" color="#b3b3b3">
-
       <IonHeader>
         <IonToolbar>
           <IonTitle>Importer les données</IonTitle>
@@ -22,14 +34,14 @@ const Import: React.FC = () => {
       </IonHeader>
 
       <IonContent>
-
         <IonList>
-
           <IonItem>
             <IonIcon icon={logIn} slot="start"></IonIcon>
             <IonLabel position="stacked">Emplacement</IonLabel>
             <IonSelect>
-              <IonSelectOption value="usb" selected>USB</IonSelectOption>
+              <IonSelectOption value="usb" selected>
+                USB
+              </IonSelectOption>
               <IonSelectOption value="cardSD">Card SD</IonSelectOption>
               <IonSelectOption value="email">Gmail</IonSelectOption>
               <IonSelectOption value="file">FileSystem</IonSelectOption>
@@ -37,7 +49,13 @@ const Import: React.FC = () => {
           </IonItem>
           <IonRow>
             <IonCol>
-              <IonButton color="light" onClick={() => setShowToast(true)} expand="block">Importer</IonButton>
+              <IonButton
+                color="light"
+                onClick={() => setShowToast(true)}
+                expand="block"
+              >
+                Importer
+              </IonButton>
               <IonToast
                 isOpen={showToast1}
                 position="middle"
@@ -46,12 +64,11 @@ const Import: React.FC = () => {
                 duration={40000}
               />
             </IonCol>
-
           </IonRow>
-
         </IonList>
       </IonContent>
 
+      <Tabs />
     </IonPage>
   );
 };
