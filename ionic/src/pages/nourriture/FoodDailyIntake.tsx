5bc8c545bf8f6097874b89b766a3a664a35c8fa8
Guerlens Robert Fontaine <guerlens.fontaine@ticksmith.com>
2019-11-23 09:56:10 -0500

    page de base pour food-daily-intake

--- ionic/src/pages/nourriture/FoodDailyIntake.tsx
+++ ionic/src/pages/nourriture/FoodDailyIntake.tsx
@@ -1,21 +1,81 @@
-
-import { Component } from 'react';
-import React from 'react';
-import { IonApp, IonHeader, IonContent, IonText } from '@ionic/react';
+import { Component } from "react";
+import React from "react";
+import {
+  IonHeader,
+  IonContent,
+  IonText,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonPage,
+  IonCard,
+  IonCardContent
+} from "@ionic/react";
 
 export default class FoodDailyIntake extends Component {
-
-    render() {
-        return (
-          <IonApp>
-            <IonHeader>
-                <IonText> FoodDailyIntake </IonText>
-            </IonHeader>
-            <IonContent>
-      
-            </IonContent>
-          </IonApp>
-        );
-      }
-
+  render() {
+    return (
+      <IonPage>
+        <IonContent>
+          <IonCard>
+            <IonCardContent>
+              <IonList>
+                <IonItem lines="full">
+                  <IonLabel>
+                    <b>Dejeuner</b>
+                  </IonLabel>
+                </IonItem>
+                <IonItem lines="none">
+                  <IonLabel>Item</IonLabel>
+                </IonItem>
+                <IonItem lines="none">
+                  <IonLabel>Item</IonLabel>
+                </IonItem>
+                <IonItem lines="full">
+                  <IonLabel>
+                    <b>Collation AM</b>
+                  </IonLabel>
+                </IonItem>
+                <IonItem lines="none">
+                  <IonLabel>Item</IonLabel>
+                </IonItem>
+                <IonItem lines="full">
+                  <IonLabel>
+                    <b>Diner</b>
+                  </IonLabel>
+                </IonItem>
+                <IonItem lines="none">
+                  <IonLabel>Item</IonLabel>
+                </IonItem>
+                <IonItem lines="full">
+                  <IonLabel>
+                    <b>Collation PM</b>
+                  </IonLabel>
+                </IonItem>
+                <IonItem lines="none">
+                  <IonLabel>Item</IonLabel>
+                </IonItem>
+                <IonItem lines="full">
+                  <IonLabel>
+                    <b>Souper</b>
+                  </IonLabel>
+                </IonItem>
+                <IonItem lines="none">
+                  <IonLabel>Item</IonLabel>
+                </IonItem>
+                <IonItem lines="full">
+                  <IonLabel>
+                    <b>Collation Soir</b>
+                  </IonLabel>
+                </IonItem>
+                <IonItem lines="none">
+                  <IonLabel>Item</IonLabel>
+                </IonItem>
+              </IonList>
+            </IonCardContent>
+          </IonCard>
+        </IonContent>
+      </IonPage>
+    );
+  }
 }
