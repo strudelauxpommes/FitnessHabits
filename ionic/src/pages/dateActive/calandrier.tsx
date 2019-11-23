28d6a19943ddaaa957dd74a1fbfc22379b8e1cfb
Samuel <castilloux.samuel@courrier.uqam.ca>
2019-11-23 15:42:22 -0500

    tabulation retoucher

--- ionic/src/pages/dateActive/calandrier.tsx
+++ ionic/src/pages/dateActive/calandrier.tsx
@@ -1,15 +1,9 @@
 import {
-  IonContent,
-  IonHeader,
-  IonPage,
-  IonTitle,
-  IonToolbar,
   IonDatetime,
-  IonButton,
   IonIcon
 } from '@ionic/react';
 import {calendar} from 'ionicons/icons';
-import './Home.css';
+import './Calendrier.css';
 import { SaveDateActive } from './StorageDate'; //GetDateActive
 import React from 'react';
 import { DalImpl } from '../../dal/DalImpl';
@@ -24,49 +18,34 @@ const dateInst: Dal = new DalImpl();
 
 
 function ParamDate(haveLimit : boolean) {
-
- 
-
-
-    var myDate = new Date();
-    var jour = myDate.getDate();
-    var mois = myDate.getMonth();
-    var annee = myDate.getFullYear();
-
-    var returnDate;
-
-    mois = mois + 1;
-
-    if (haveLimit) {
-        if (mois <= 3) {
-            mois = mois + 12;
-            annee = annee - 1;
-        }
-        mois = mois - 3;
-       
+  var myDate = new Date();
+  var jour = myDate.getDate();
+  var mois = myDate.getMonth();
+  var annee = myDate.getFullYear();
+  var returnDate;
+  mois = mois + 1;
+  if (haveLimit) {
+    if (mois <= 3) {
+      mois = mois + 12;
+      annee = annee - 1;
     }
-
-    returnDate = annee + '-';
-
-    if (mois < 10) {
-        returnDate = returnDate + '0';
-    }
-
-    returnDate = returnDate + mois + '-';
-
-    if (jour < 10) {
-        returnDate = returnDate + '0';
-    }
-
+    mois = mois - 3;
+  }
+  returnDate = annee + '-';
+  if (mois < 10) {
+    returnDate = returnDate + '0';
+  }
+  returnDate = returnDate + mois + '-';
+  if (jour < 10) {
+    returnDate = returnDate + '0';
+  }
     
-    return returnDate + jour;
-
-
+  return returnDate + jour;
 }
 
 
 
-const Home: React.FC = () => {
+const Calendrier: React.FC = () => {
   return (
             <IonIcon icon={calendar}></IonIcon>
             <IonDatetime displayFormat="YYYY/MM/DD" min={ParamDate(true)} max={ParamDate(false)} value={myDate.toString()}
@@ -75,11 +54,10 @@ const Home: React.FC = () => {
               var moisActu = utcDate.getMonth()+1;
               var anneActu = utcDate.getFullYear();
               var tab = [dateActu,moisActu,anneActu];
-              
               await dateInst.setValue(cleDate, tab);
-            }
-                }/>                    
+              }
+              }/>
   );
 };
 
-export default Home;
+export default Calendrier;
