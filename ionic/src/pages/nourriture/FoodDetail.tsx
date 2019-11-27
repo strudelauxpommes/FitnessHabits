ee85f8a9bc007d1d0dcf7c8da45c383428c43e3a
Toquart <davidmaignan@gmail.com>
2019-11-27 17:21:31 -0500

    Remove unused react pie chart import

--- ionic/src/pages/nourriture/FoodDetail.tsx
+++ ionic/src/pages/nourriture/FoodDetail.tsx
@@ -14,7 +14,6 @@ import {
 } from "@ionic/react";
 import { FoodHeader } from "./FoodHeader";
 import { Pie } from "react-chartjs-2";
-import PieChart from "react-minimal-pie-chart";
 // import { Ionicons } from '@expo/vector-icons';
 let state = {
   dataPie: {
